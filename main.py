3fdb750d5ae91efef0ff2dd1a0e771f3a722a8a0
Humphrey Dulo <106704855+HumphreyDulo@users.noreply.github.com>
2023-10-01 19:29:57 +0300

    Changed comments

--- main.py
+++ main.py
@@ -4,29 +4,32 @@ from functions import copy_jsonl_files, create_excel_files, process_excel_files,
 
 
 def main():
-    # Update the project directory
+    """
+    1.Update the project directory,
+    2.Import extracted JSONL files
+    3.Generate Excel files from JSONL files,
+    4.Generate test, train, and dev JSONL files for select languages
+    5.Generate a translations.json file
+    """
+
     project_dir = 'C:/Users/User/PycharmProjects/Computer-Graphics-GRP-6'
 
-    # Step 1: Copy JSONL files from source to destination directory
     source_dir = 'C:/Users/User/Desktop/New folder'
     destination_dir = os.path.join(project_dir, 'JSONL_Files')
     os.makedirs(destination_dir, exist_ok=True)
     copy_jsonl_files(source_dir, destination_dir)
 
-    # Step 2: Generate Excel files from JSONL files
     jsonl_dir = os.path.join(project_dir, 'JSONL_Files')
     output_dir = os.path.join(project_dir, 'Excel_Files')
     os.makedirs(output_dir, exist_ok=True)
     create_excel_files(jsonl_dir, output_dir)
 
-    # Step 3: Generate test, train, and dev JSONL files for select languages
     excel_dir = output_dir
     jsonl_output_dir = os.path.join(project_dir, 'JSONL_Output')
     os.makedirs(jsonl_output_dir, exist_ok=True)
     process_excel_files(excel_dir, jsonl_output_dir)
 
-    # Step 4: Generate a translations.json file
-    generate_translations_json(jsonl_output_dir, os.path.join(project_dir, 'translations/translations.json'))
+    generate_translations_json(jsonl_output_dir, os.path.join(project_dir, 'translations.json'))
 
 
 if __name__ == "__main__":
