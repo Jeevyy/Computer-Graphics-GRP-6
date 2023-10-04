aabb14c69a9941afd22667098ddd9d3e8e414c99
Amanvasani <aman.vasani@strathmore.edu>
2023-10-04 09:51:15 +0300

    Comment cleanup

--- google_drive.py
+++ google_drive.py
@@ -8,14 +8,14 @@ translations_file_path = 'C:/Users/User/PycharmProjects/Computer-Graphics-GRP-6/
 jsonl_output_dir = 'C:/Users/User/PycharmProjects/Computer-Graphics-GRP-6/JSONL_Output'
 excel_files_dir = 'C:/Users/User/PycharmProjects/Computer-Graphics-GRP-6/Excel_Files'
 
-"""Path to your Google API credentials JSON file"""
+"""This is the Path to the Google API credentials JSON file in the local computer"""
 credentials_file_path = 'C:/Users/User/Downloads/group-5-400916-b4f38ed28334.json'
 
-"""Load Google API credentials"""
+"""Load the Google API credentials"""
 credentials = service_account.Credentials.from_service_account_file(credentials_file_path,
                                                                     scopes=['https://www.googleapis.com/auth/drive'])
 
-"""Build the Google Drive API service"""
+"""The below code builds the Google Drive API service"""
 drive_service = build('drive', 'v3', credentials=credentials)
 
 
@@ -36,14 +36,15 @@ def get_folder_id_by_name(folder_name):
         return folder.get('id')
 
 
-# Get the ID of the "group6" folder or create it if it doesn't exist
+"""Get the ID of the "group6" folder or create it if it doesn't exist"""
 group6_folder_id = get_folder_id_by_name("group6")
 
 
 def upload_to_drive(local_file_path, drive_folder_id=None):
     # Define the file metadata
     file_metadata = {
-        'name': os.path.basename(local_file_path),  # Name of the file on Google Drive
+        """Name of the file on Google Drive"""
+        'name': os.path.basename(local_file_path),
     }
 
     if drive_folder_id:
@@ -60,7 +61,7 @@ def upload_to_drive(local_file_path, drive_folder_id=None):
     return file_id
 
 
-"""translations.json file to "group6" folder"""
+"""translations.json file to the "group6" folder"""
 translations_file_id = upload_to_drive(translations_file_path, group6_folder_id)
 
 """files in the JSONL_Output directory """
